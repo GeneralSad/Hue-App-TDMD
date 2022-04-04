92c18da411a39925ec85f2eb73d627840dab9c22
Luuk Verhagen <l.verhagen5@student.avans.nl>
2022-04-04 15:00:53 +0200

    Added GroupsFragment

---
+++ app/src/main/java/com/leonv/hueapp/GroupsFragment.java
@@ -0,0 +1,24 @@
+package com.leonv.hueapp;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+
+public class GroupsFragment extends Fragment {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+    }
+
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+
+        return inflater.inflate(R.layout.fragment_groups, container, false);
+    }
+}
